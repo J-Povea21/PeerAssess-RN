bd18a9d591c1b65fc00712270fa503c80005eebf
victor cantillo <vdcantillo@uninorte.edu.co>
2026-05-01 19:01:07 -0500

    [86b9kxbb1]: implement members tab with deduplicated course members list

--- src/navigation/StudentTabsNavigator.tsx
+++ src/navigation/StudentTabsNavigator.tsx
@@ -1,25 +1,20 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesWrapper from "../features/groups/presentation/screens/CategoriesWrapper";
 import HomeScreen from "../features/home/presentation/HomeScreen";
 import GroupListScreen from "../features/groups/presentation/screens/GroupListScreen";
+import MembersTab from "../features/groups/presentation/screens/MembersTab";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-
 function DashboardStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        name="GroupList"
-        component={GroupListScreen}
-      />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="GroupList" component={GroupListScreen} />
     </Stack.Navigator>
   );
 }
@@ -27,22 +22,15 @@ function DashboardStack() {
 export default function StudentTabsNavigator() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen
-        name="Dashboard"
-        component={DashboardStack}
-      />
-      <Tab.Screen
-  name="Courses"
-  component={CategoriesWrapper}
-/>
-      <Tab.Screen
-        name="Results"
-        component={HomeScreen}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={HomeScreen}
-      />
+      <Tab.Screen name="Dashboard" component={DashboardStack} />
+
+      <Tab.Screen name="Courses" component={CategoriesWrapper} />
+
+    
+      <Tab.Screen name="Members" component={MembersTab} />
+
+      <Tab.Screen name="Results" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={HomeScreen} />
     </Tab.Navigator>
   );
 }
